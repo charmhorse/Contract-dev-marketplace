61164550cba2635fda267c8ecf9bde3a0390520a
charmhorse <lassenygaard.dev@gmail.com>
2024-04-19 12:42:31 +0100

    feat/new

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
-const path = require('path')
+const withImages = require('next-images');
 
-module.exports = {
-  trailingSlash: true,
-  reactStrictMode: false,
-  experimental: {
-    esmExternals: false,
-    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
-  },
-  webpack: config => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
-    }
-
-    return config
+const redirects = {
+  async redirects() {
+    return [
+      {
+        source: '/dashboards',
+        destination: '/dashboards/crypto',
+        permanent: true
+      }
+    ];
   }
-}
+};
+
+module.exports = withImages(redirects);
